a13044d7808e13dd19e3540e362b520fcae73bd7
edasaner <esaner02@gmail.com>
2023-08-09 10:17:35 +0200

    Fix gaps in bookshelf grid
    Filtering bookshelf items was creating gaps that now have been fixed.

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// function filterbookshelf(e) {
-//     const selection = document.querySelectorAll(".bookshelf article");
-//     let filter = e.target.dataset.filter;
-//     selection.forEach(selection => {
-//         selection.classList.contains(filter) ?
-//             selection.classList.remove('hidden') :
-//             selection.classList.add('hidden');
-//     });
-// };
-
+// Filter Books
 function filterbookshelf(e) {
     const selection = document.querySelectorAll(".hover-wrapper");
     let filter = e.target.dataset.filter;
@@ -18,25 +9,6 @@ function filterbookshelf(e) {
 
         } else {
             item.style.display = 'none';
-            // document.getElementsByClassName("placeholder").style.display = flex;
         }
     });
 }
-
-// if (filter === "*") {
-//     bookshelf.forEach(bookshelf => bookshelf.classList.remove("hidden"));
-// }
-
-// function filterbookshelf(e) {
-//     const bookshelf = document.querySelectorAll(".list div");
-//     let filter = e.target.dataset.filter;
-//     if (filter === "*") {
-//         bookshelf.forEach(bookshelf => bookshelf.classList.remove("hidden"));
-//     } else {
-//         bookshelf.forEach(animal => {
-//             bookshelf.classList.contains(filter) ?
-//                 bookshelf.classList.remove("hidden") : animal
-//             bookshelf.classList.add("hidden");
-//         })
-//     }
-// }
